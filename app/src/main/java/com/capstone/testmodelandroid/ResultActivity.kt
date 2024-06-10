e9fc7210e9f1568fcb2e057b3ddbcf91ecc1a5eb
Airi <airiagustus82@gmail.com>
2024-06-11 02:32:15 +0700

    add activity and list view for output from model

--- app/src/main/java/com/capstone/testmodelandroid/ResultActivity.kt
+++ app/src/main/java/com/capstone/testmodelandroid/ResultActivity.kt
@@ -1,20 +1,33 @@
 package com.capstone.testmodelandroid
 
 import android.os.Bundle
+import android.widget.ArrayAdapter
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.capstone.testmodelandroid.databinding.ActivityResultBinding
 
 class ResultActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityResultBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_result)
+        binding = ActivityResultBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        val recipes = intent.getStringArrayExtra(RECIPES) ?: arrayOf()
+
+        val adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, recipes)
+        binding.listViewRecipes.adapter = adapter
+    }
+
+    companion object {
+        private const val RECIPES = "recipes"
     }
 }
